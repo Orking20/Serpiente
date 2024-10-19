cd7db4a73d7f048a6898df6206f509af839b3efa
Orking20 <mateoluchetti@hotmail.com>
2024-10-20 00:17:01 +0200

    Comienzo del proyecto

---
+++ scripts/menu_principal.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class menu_principal : Control
+{
+	Button btnJugar;
+	Button btnSalir;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.btnJugar = GetNode<Button>("ContenedorBotones/BtnJugar");
+		this.btnSalir = GetNode<Button>("ContenedorBotones/BtnSalir");
+
+		this.btnJugar.Pressed += OnBotonJugarPressed;
+		this.btnSalir.Pressed += OnBotonSalirPressed;
+	}
+
+	private void OnBotonJugarPressed()
+	{
+		GetTree().ChangeSceneToFile("res://escenas/nivel_1");
+	}
+
+	private void OnBotonSalirPressed()
+	{
+		GetTree().Quit();
+	}
+}
